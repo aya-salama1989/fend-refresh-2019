eca1ef0c80fb497f1cb1905c141a225d2b54237f
ayasalama89 <ayoyasalama@gmail.com>
2021-05-21 06:05:37 +0200

    use intersectionobserver instead of viewbounds

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -14,15 +14,15 @@ const sections = document.querySelectorAll("section[data-nav]");
 *
 */
 
-let isInViewport = function (elem) {
-  let bounding = elem.getBoundingClientRect();
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
+// let isInViewport = function (elem) {
+//   let bounding = elem.getBoundingClientRect();
+//   return (
+//     bounding.top >= 0 &&
+//     bounding.left >= 0 &&
+//     bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// };
 
 
 /**
@@ -48,22 +48,47 @@ menuUlElement.appendChild(fragment);
 
 // Add class 'active' to section when near top of viewport
 
-document.addEventListener("scroll", function(){
-  sections.forEach((item, i) => {
-    let sectionClassList = sections[i].classList;
-    let devClassList = sections[i].querySelector('div').classList;
+// document.addEventListener("scroll", function(){
+//   sections.forEach((item, i) => {
+//     let sectionClassList = sections[i].classList;
+//     let devClassList = sections[i].querySelector('div').classList;
+//
+//     if(isInViewport(sections[i])){
+//       //highlite the paragraph
+//       sectionClassList.add("your-active-class");
+//       devClassList.add("landing__container::before", "landing__container::after");
+//       menuUlElement.querySelectorAll('li')[i].querySelector('a').classList.add("activeState");
+//     }else {
+//       sectionClassList.remove("your-active-class");
+//       devClassList.remove("landing__container::before", "landing__container::after");
+//       menuUlElement.querySelectorAll('li')[i].querySelector('a').classList.remove("activeState");
+//     }
+//   });
+// });
 
-    if(isInViewport(sections[i])){
-      //highlite the paragraph
-      sectionClassList.add("your-active-class");
-      devClassList.add("landing__container::before", "landing__container::after");
-      menuUlElement.querySelectorAll('li')[i].querySelector('a').classList.add("activeState");
-    }else {
-      sectionClassList.remove("your-active-class");
-      devClassList.remove("landing__container::before", "landing__container::after");
-      menuUlElement.querySelectorAll('li')[i].querySelector('a').classList.remove("activeState");
-    }
+// the callback function that will be fired
+// when the element apears in the viewport
+function onEntry(entry) {
+  let menuElements = menuUlElement.querySelectorAll('li')
+  entry.forEach((change, i) => {
+    change.target.classList.add("your-active-class");
+    change.target.classList.add("landing__container::before", "landing__container::after");
+    menuElements[i].querySelector('a').classList.add("activeState");
   });
+}
+
+// list of options
+let options = {
+  threshold: [0.1]
+};
+
+// instantiate a new Intersection Observer
+let observer = new IntersectionObserver(onEntry, options);
+
+// loop through all elements
+// pass each element to observe method
+sections.forEach((item, i) => {
+  observer.observe(item);
 });
 
 
